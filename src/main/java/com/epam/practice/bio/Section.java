4fdac062fd676905bb3275a7a3305b1968e7fabb
Ilia Vertiletskii <Ilia_vertiletskii@epam.com>
2019-03-31 17:17:57 +0300

    SOLUTION

--- src/main/java/com/epam/practice/bio/Section.java
+++ src/main/java/com/epam/practice/bio/Section.java
@@ -3,13 +3,26 @@ package com.epam.practice.bio;
 import java.util.Objects;
 
 public class Section {
+    int n;
+    String content;
 
-    // TODO implement this class
+    public Section(int n, String content) {
+        this.n = n;
+        this.content = content;
+    }
+
+    public Section() {
+    }
 
     @Override
     public boolean equals(Object other) {
         return this == other
             || other != null && getClass() == other.getClass()
             && Objects.equals(this.toString(), other.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(n, content);
     }
 }
--- src/main/java/com/epam/practice/bio/Solver.java
+++ src/main/java/com/epam/practice/bio/Solver.java
@@ -1,13 +1,38 @@
 package com.epam.practice.bio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Solver {
 
+    public Solver() {
+    }
+
     public Map<Section, List<Integer>> analyze(File input) throws IOException {
-        throw new UnsupportedOperationException();
+        FileReader fileReader = new FileReader(input);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        Map<Section, List<Integer>> result = new HashMap<>();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String line = bufferedReader.readLine();
+
+
+        for (int i = 0; i < line.length() - 1; i++) {
+            String substring = line.substring(i, i + n);
+            Section section = new Section(n, substring);
+
+            List<Integer> indexes = result.getOrDefault(section, new ArrayList<>());
+            indexes.add(i);
+            result.put(section, indexes);
+
+        }
+
+        return result;
     }
 }
