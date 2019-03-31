4cd6b10b48aaa20e116eee4af9a324d90a5eb1a4
Ignat_Bavtovich <ignat_bavtovich@epam.com>
2019-03-31 16:25:46 +0300

    init

--- src/main/java/com/epam/practice/bio/Solver.java
+++ src/main/java/com/epam/practice/bio/Solver.java
@@ -1,13 +1,25 @@
 package com.epam.practice.bio;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Solver {
 
     public Map<Section, List<Integer>> analyze(File input) throws IOException {
+        BufferedReader re = new BufferedReader(new FileReader(input));
+        String string = re.readLine();
+        if (string == null) {
+            return Collections.emptyMap();
+        }
+        Map<Section, List<Integer>> result = getResult(string, ThreadLocalRandom.current().nextInt(5, 1001));
+
         throw new UnsupportedOperationException();
+    }
+
+    public Map<Section, List<Integer>> getResult(String input, int subLength) {
+        return Collections.emptyMap();
     }
 }
